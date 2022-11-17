bdca2c32002563c1c3bfdc78990ecb95e3ad46e6
Angel Veliz-Castro <44382529+AVC12345@users.noreply.github.com>
2022-11-17 16:34:12 -0500

    Created Staff Nav

--- public_html/content/adminnav.php
+++ public_html/content/adminnav.php
@@ -31,7 +31,7 @@
     <hr class="dropdown-divider mt-sm-3 mt-md-0 d-md-none d-lg-none d-lx-none d-xxl-none">
     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <a class="nav-link link-color" aria-current="page" href="#">Log Out</a>
+            <a class="nav-link link-color" aria-current="page" href="logout.php">Log Out</a>
         </li>
     </ul>
 </div>
