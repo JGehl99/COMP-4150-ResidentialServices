0666c466c0a338c391a52ba102897b159548ff74
Angel Veliz-Castro <44382529+AVC12345@users.noreply.github.com>
2022-11-17 16:36:19 -0500

    Created Staff Nav

---
+++ public_html/content/staffnav.php
@@ -0,0 +1,19 @@
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link link-color" aria-current="page" href="#">View Student Flats</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-color" href="#">View Flat Inspections</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-color" href="#">Add Flat Inspections</a>
+        </li>
+    </ul>
+    <hr class="dropdown-divider mt-sm-3 mt-md-0 d-md-none d-lg-none d-lx-none d-xxl-none">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link link-color" aria-current="page" href="logout.php">Log Out</a>
+        </li>
+    </ul>
+</div>
